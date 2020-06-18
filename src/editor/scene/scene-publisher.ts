42ba461338fac7d34e572c5dfe491037ffd5084b
jkeys <jkeys@ecg-hq.com>
2020-06-17 20:58:38 -0600

    first pass at scene-publisher.ts using scene-serializer.ts as a scaffold

---
+++ src/editor/scene/scene-publisher.ts
@@ -0,0 +1,60 @@
+import {
+    Scene, Mesh,
+    Tools as BabylonTools,
+    SceneSerializer as BabylonSceneSerializer } from 'babylonjs';
+import { GLTF2Export, OBJExport } from 'babylonjs-serializers';
+
+import Tools from '../tools/tools';
+
+import Window from '../gui/window';
+import Form from '../gui/form';
+
+export default class ScenePublisher {
+    /**
+     * Constructor
+     * @param scene: the scene to publish
+     */
+    constructor (scene: Scene) {
+        // Create window
+        const window = new Window('Scene Publisher');
+        window.buttons = ['Publish', 'Cancel'];
+        window.width = 400;
+        window.height = 190;
+        window.body = `<div id="SCENE-SERIALIZER-WINDOW" style="width: 100%; height: 100%"></div>`;
+        window.open();
+
+        // Form
+        const form = new Form('ScenePublisher');
+        form.fields = [
+            { name: 'profile name', type: 'text', required: true },
+            { name: 'file path', type: 'text', required: true },
+            { name: 'provider', type: 'list', required: true, options: { items: ['AWS',] } },
+            { name: 'distribution ID', type: 'text', required: false }
+        ];
+        form.build('SCENE-PUBLISHER-WINDOW');
+
+        // Set default values
+        form.element.record['name'] = 'scene';
+        form.element.record['profile name'] = 'default';
+        form.element.refresh();
+
+        // Events
+        window.onButtonClick = async (id) => {
+            window.close();
+
+            if (id === 'Cancel')
+                return form.element.destroy();
+
+            if (!form.isValid())
+                return;
+            
+            const name = form.element.record['name'];
+            const filePath = form.element.record['file path'];
+            const provider = form.element.record['provider'];
+            const distributionId = form.element.record['distribution ID']
+
+            // Clear
+            form.element.destroy();
+        };
+    }
+}
