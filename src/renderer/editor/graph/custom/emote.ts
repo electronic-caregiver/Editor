84245ed9c62ea35bdda484a5cfa50c8ef7a500c6
jkeys <jkeys@ecg-hq.com>
2020-08-15 20:41:16 -0600

    update to Julien's suggested code

--- src/renderer/editor/graph/custom/emote.ts
+++ src/renderer/editor/graph/custom/emote.ts
@@ -1,3 +1,4 @@
+import { TransformNode} from 'babylonjs';
 import { LiteGraph } from "litegraph.js";
 
 import {
@@ -7,68 +8,41 @@ import {
 } from "../node";
 
 export class Emote extends GraphNode {
-  /**
-   * Constructor.
-   */
-  public constructor() {
-    super("Emote");
+    /**
+     * Constructor.
+     */
+    public constructor() {
+      super("Emote");
 
-    // Let's add our first input which is the "trigger" pin
-    this.addInput(
-      "", // This is the name of the input. We don't really set a name for the trigger input as it is visually understandable.
-      LiteGraph.EVENT as any // "as any" is mandatory as litegraph.js still has problems in its typings.
-    );
-    
-    this.addProperty(
-      "Emote name",
-      "vannaMid",
-      "string",
-      (v) => (this.properties.emote_name = v)
-    );
-    this.addProperty(
-      "Dummy Node Name",
-      "HostDummyNode",
-      "string",
-      (v) => (this.properties.node_name = v)
-    );
+      this.addInput("", LiteGraph.EVENT as any);
+      this.addInput("node *", "TransformNode");
 
-    this.addWidget("text", "emote_name", this.properties.emote_name, (v) => {
-      this.properties.emote_name = v;
-    });
-    this.addWidget("text", "node_name", this.properties.node_name, (v) => {
-      this.properties.node_name = v;
-    });
+      this.addProperty("emote_name", "vannaMid", "string", (v) => this.properties.emote_name = v);
 
-    this.addOutput("", LiteGraph.EVENT as any);
+      this.addWidget("text", "emote_name", this.properties.emote_name, (v) => this.properties.emote_name = v);
+
+      this.addOutput("", LiteGraph.EVENT as any);
   }
 
   /**
    * Called on the node is being executed.
    */
   public execute(): void {
-    const node = this.getScene().getNodeByName(this.properties.node_name);
+      const node = this.getInputData<TransformNode>(1);
+      if (!node?.metadata?.host) { return; }
 
-    const host = node && node.metadata.host;
+      node.metadata.host.GestureFeature.playGesture("Emote", this.properties.emote_name);
 
-    const emote = this.properties.emote_name;
-
-    host.GestureFeature.playGesture("Emote", emote);
-
-    this.triggerSlot(0, null);
+      this.triggerSlot(0, null);
   }
 
   /**
    * Generates the code of the graph.
    */
-  public generateCode(): ICodeGenerationOutput {
-    const code = `
-        const node = this._scene.getNodeByName('${this.properties.node_name}');
-        node.metadata.host.GestureFeature.playGesture("Emote", '${this.properties.emote_name}');
-    `;
-
-    return {
-      type: CodeGenerationOutputType.Function,
-      code,
-    };
+  public generateCode(node: ICodeGenerationOutput): ICodeGenerationOutput {
+      return {
+          type: CodeGenerationOutputType.Function,
+          code: `${node.code}.metadata.host.GestureFeature.playGesture("Emote", "${this.properties.emote_name}")`
+      };
   }
 }
